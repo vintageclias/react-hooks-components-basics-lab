b6ca0d75bc55a079c88863a208b0c602c22681b8
vintageclias <vintageclias@gmail.com>
2024-11-05 05:02:28 -0800

    basic-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,27 @@
-import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
 
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
-  );
-}
+import React from 'react';  
+import Navbar from './Navbar'; 
+import Home from './Home';     
 
-export default App;
+
+const About = () => {  
+  return (  
+    <div id="about">  
+      <h2>About</h2>  
+    </div>  
+  );  
+};  
+
+
+const App = () => {  
+  return (  
+    <div>  
+      <Navbar /> 
+      <Home />    
+      <About />   
+    </div>  
+  );  
+};  
+
+export default App;  
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react';  
+
+const Navbar = () => {  
+  return (  
+    <nav>  
+      <h1>My Portfolio</h1> 
+    </nav>  
+  );  
+};  
+
+export default Navbar;  
